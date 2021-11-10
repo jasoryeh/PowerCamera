70cd78c68867994ef9474f7cdbe4ddd2edcffa8e
Jason <git@hogt.me>
2021-11-10 13:38:41 -0800

    Fix stats command

--- src/main/java/nl/svenar/powercamera/commands/cmd_stats.java
+++ src/main/java/nl/svenar/powercamera/commands/cmd_stats.java
@@ -1,12 +1,13 @@
 package nl.svenar.powercamera.commands;
 
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.TimeZone;
 import nl.svenar.powercamera.PowerCamera;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
 
 public class cmd_stats extends PowerCameraCommand {
 
@@ -14,47 +15,46 @@ public class cmd_stats extends PowerCameraCommand {
 		super(plugin, command_name, COMMAND_EXECUTOR.ALL);
 	}
 
-	private void showStats(PowerCommandSender sender) {
-		if (sender.hasPermission(PowerCameraPermissions.CMD_STOP)) {
-			if (this.plugin.player_camera_mode.get(((Player) sender.getSender()).getUniqueId()) != null && this.plugin.player_camera_mode.get(((Player) sender.getSender()).getUniqueId()) != PowerCamera.CAMERA_MODE.NONE && this.plugin.player_camera_handler.get(((Player) sender.getSender()).getUniqueId()) != null) {
-				this.plugin.player_camera_handler.get(((Player) sender.getSender()).getUniqueId()).stop();
-				if (!sender.hasPermission(PowerCameraPermissions.HIDESTARTMESSAGES)) sender.sendMessage(ChatColor.GREEN + "Current camera stopped");
-			} else {
-				sender.sendMessage(ChatColor.RED + "No camera active!");
-			}
-		} else {
-			sender.sendMessage(ChatColor.DARK_RED + "You do not have permission to execute this command");
-		}
-	}
-
-	@Override
-	public boolean onCommand(PowerCommandSender sender, Command cmd, String commandLabel, String[] args) {
-		if (!sender.hasPermission(PowerCameraPermissions.CMD_STATS)) {
-			sender.sendMessage(ChatColor.DARK_RED + "You do not have permission to execute this command");
-			return false;
-		}
-
-		if (args.length != 0) {
-			sender.sendMessage(ChatColor.DARK_RED + "Usage: /" + commandLabel + " stats");
-			return false;
-		}
-
+	public void showStats(PowerCommandSender sender) {
 		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
 		format.setTimeZone(TimeZone.getTimeZone("UTC"));
 		Instant current_time = Instant.now();
 
 		String invisibility_mode = "None";
 
-		if (this.plugin.getConfigPlugin().shouldUseSpectator() && this.plugin.getConfigPlugin().shouldUseInvisibility()) {
+		if (this.plugin.getConfigPlugin().getConfig().getBoolean("camera-effects.spectator-mode") && this.plugin.getConfigPlugin().getConfig().getBoolean("camera-effects.invisible")) {
 			invisibility_mode = "spectator & invisible";
 		} else {
-			if (this.plugin.getConfigPlugin().shouldUseSpectator()) {
+			if (this.plugin.getConfigPlugin().getConfig().getBoolean("camera-effects.spectator-mode")) {
 				invisibility_mode = "specator";
 			}
 
-			if (this.plugin.getConfigPlugin().shouldUseInvisibility()) {
+			if (this.plugin.getConfigPlugin().getConfig().getBoolean("camera-effects.invisible")) {
 				invisibility_mode = "invisible";
 			}
+		}
+
+		sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "----------" + ChatColor.AQUA + plugin.getPluginDescriptionFile().getName() + ChatColor.DARK_AQUA + "----------" + ChatColor.BLUE + "===");
+		sender.sendMessage(ChatColor.DARK_GREEN + "Server version: " + ChatColor.GREEN + Bukkit.getVersion());
+		sender.sendMessage(ChatColor.DARK_GREEN + "Bukkit version: " + ChatColor.GREEN + Bukkit.getServer().getBukkitVersion());
+		sender.sendMessage(ChatColor.DARK_GREEN + "Java version: " + ChatColor.GREEN + System.getProperty("java.version"));
+		sender.sendMessage(ChatColor.DARK_GREEN + plugin.getPluginDescriptionFile().getName() + " Version: " + ChatColor.GREEN + plugin.getPluginDescriptionFile().getVersion());
+		sender.sendMessage(ChatColor.DARK_GREEN + "Plugin uptime: " + ChatColor.GREEN + format.format(Duration.between(plugin.powercamera_start_time, current_time).toMillis()));
+		sender.sendMessage(ChatColor.DARK_GREEN + "Registered cameras: " + ChatColor.GREEN + plugin.getConfigCameras().getCameras().size());
+		sender.sendMessage(ChatColor.DARK_GREEN + "Registered players: " + ChatColor.GREEN + plugin.getConfigCameras().getPlayers().size());
+		sender.sendMessage(ChatColor.DARK_GREEN + "Invisibility mode: " + ChatColor.GREEN + invisibility_mode);
+		sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "-------------------------------" + ChatColor.BLUE + "===");
+	}
+
+	@Override
+	public boolean onCommand(PowerCommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (!sender.hasPermission(PowerCameraPermissions.CMD_STATS)) {
+			sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.DARK_RED + "You do not have permission to execute this command");
+			return false;
+		}
+		if (args.length != 0) {
+			sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.DARK_RED + "Usage: /" + commandLabel + " stats");
+			return false;
 		}
 
 		this.showStats(sender);
